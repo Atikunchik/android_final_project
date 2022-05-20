b7c88184bd3edb807b2507030a3c178715f4bf10
Albert <albert.zhumashev@gmail.com>
2022-05-20 11:13:40 +0600

    ended

--- final_project/app/src/main/java/com/federicocotogno/retrofit2example/api/CovidApi.kt
+++
@@ -1,7 +0,0 @@
-package com.federicocotogno.retrofit2example.api
-
-data class Country(
-    val Country: String,
-    val Slug: String,
-    val ISO2: String
-)
--- final_project/app/src/main/java/com/federicocotogno/retrofit2example/api/Status.kt
+++
@@ -1,6 +0,0 @@
-package com.federicocotogno.retrofit2example.api
-
-data class Status(
-    val sentCount: Int,
-    val verified: Boolean
-)
